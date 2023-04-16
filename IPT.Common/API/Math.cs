7ba532de6cf4edf1f1a55c9e9888cf9d8d2598c8
puska <puska@DESKTOP-2K38EAL>
2023-04-16 13:07:25 -0500

    added angle and heading calculations

--- IPT.Common/API/Math.cs
+++ IPT.Common/API/Math.cs
@@ -1,4 +1,6 @@
-﻿namespace IPT.Common.API
+﻿using Rage;
+
+namespace IPT.Common.API
 {
     /// <summary>
     /// Additional math-related functions.
@@ -80,6 +82,48 @@
             }
 
             return cValue;
+        }
+
+        /// <summary>
+        /// Calculate the angle in degrees from the front of the origin entity to the target entity.
+        /// </summary>
+        /// <param name="origin">The origin entity.</param>
+        /// <param name="target">The target entity.</param>
+        /// <returns>A float value representing the smallest angle in degrees between the origin and the target.</returns>
+        public static float CalculateAngleToTarget(Entity origin, Entity target)
+        {
+            var heading = CalculateHeading(origin.Position, target.Position);
+            var offset = System.Math.Abs(origin.Heading - heading);
+            return offset <= 180 ? offset : 360 - offset;
+        }
+
+        /// <summary>
+        /// Calculates the heading angle in degrees from the origin point to the target point in 2D space.
+        /// The angle is measured counterclockwise from the positive Y-axis such that:
+        ///     North = 0
+        ///     East = 270
+        ///     South = 180
+        ///     West = 90
+        /// The positive-Y axis is used as zero because that is what Rage uses for entity headings.
+        /// </summary>
+        /// <param name="origin">The origin point.</param>
+        /// <param name="target">The target point.</param>
+        /// <returns>A float value representing the heading angle in degrees between the two points.</returns>
+        public static float CalculateHeading(Vector3 origin, Vector3 target)
+        {
+            var radians = System.Math.Atan2(target.Y - origin.Y, target.X - origin.X);
+            var degrees = ConvertRadiansToDegrees(radians) - 90.0;
+            return (float)(degrees >= 0 ? degrees : degrees + 360.0);
+        }
+
+        /// <summary>
+        /// Converts radians into degrees.
+        /// </summary>
+        /// <param name="radians">The initial value in radians.</param>
+        /// <returns>A double value reprensting the radians in degrees.</returns>
+        public static double ConvertRadiansToDegrees(double radians)
+        {
+            return radians * (180 / System.Math.PI);
         }
     }
 }
