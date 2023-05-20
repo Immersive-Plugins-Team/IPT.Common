bdf484b974127abacd8d1ed198779e4171a2587c
puska <puska@DESKTOP-2K38EAL>
2023-05-20 13:54:23 -0500

    added weather

---
+++ IPT.Common/API/MathHelperExtensions.cs
@@ -0,0 +1,28 @@
+﻿namespace IPT.Common.API
+{
+    /// <summary>
+    /// Math helper.
+    /// </summary>
+    public static class MathHelperExtensions
+    {
+        /// <summary>
+        /// Wraps an angle.
+        /// </summary>
+        /// <param name="angle">The angle.</param>
+        /// <returns>The wrapped angle.</returns>
+        public static float WrapAngle(float angle)
+        {
+            angle %= 360f;
+            if (angle > 180f)
+            {
+                angle -= 360f;
+            }
+            else if (angle < -180f)
+            {
+                angle += 360f;
+            }
+
+            return angle;
+        }
+    }
+}
--- IPT.Common/Enums.cs
+++ IPT.Common/Enums.cs
@@ -19,5 +19,27 @@
         ReturnToStation,
         TrafficStop,
     }
+
+    /// <summary>
+    /// Represents a weather type.
+    /// </summary>
+    public enum EWeatherType
+    {
+        Blizzard = 669657108,
+        Christmas = -1429616491,
+        Clear = 916995460,
+        Clearing = 1840358669,
+        Clouds = 821931868,
+        ExtraSunny = -1750463879,
+        Foggy = -1368164796,
+        Halloween = -921030142,
+        LightSnow = 603685163,
+        Neutral = -1530260698,
+        Overcast = -1148613331,
+        Rain = 1420204096,
+        Smog = 282916021,
+        Snow = -273223690,
+        Thunder = -1233681761,
+    }
 #pragma warning restore 1591, SA1602
 }
